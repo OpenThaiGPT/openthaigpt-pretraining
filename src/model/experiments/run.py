0481cdf64cbff935e6d5ef618a36d7c494334d45
srn-source <surapon22062@gmail.com>
2023-04-30 00:22:22 +0900

    move_folder_and_add_dependency

--- src/model/experiments/run.py
+++ src/model/experiments/run.py
@@ -14,11 +14,14 @@ from datasets import load_dataset
 from transformers import GPT2TokenizerFast
 from transformers.models.gpt2.modeling_gpt2 import GPT2Attention
 
-from typing import Tuple, Optional, Callable
-from torch.optim.optimizer import Optimizer
+from lion_pytorch import Lion
 
-from model import make_model, _attn_wrapper, _attn_orig
-from constants import (
+from openthaigpt_pretraining_model.nanoGPT.model import (
+    make_model,
+    _attn_wrapper,
+    _attn_orig,
+)
+from openthaigpt_pretraining_model.nanoGPT.constants import (
     MODEL_NAME,
     BOS_TOKEN,
     EOS_TOKEN,
@@ -40,70 +43,6 @@ def seed_everything(seed):
     torch.backends.cudnn.benchmark = False
     np.random.seed(seed)
     random.seed(seed)
-
-
-# functions
-
-
-def exists(val):
-    return val is not None
-
-
-# update functions
-def update_fn(p, grad, exp_avg, lr, wd, beta1, beta2):
-    # stepweight decay
-    p.data.mul_(1 - lr * wd)
-    # weight update
-    update = exp_avg.clone().mul_(beta1).add(grad, alpha=1 - beta1).sign_()
-    p.add_(update, alpha=-lr)
-
-    # decay the momentum running average coefficient
-    exp_avg.mul_(beta2).add_(grad, alpha=1 - beta2)
-
-
-# class
-
-
-class Lion(Optimizer):
-    def __init__(
-        self,
-        params,
-        lr: float = 1e-4,
-        betas: Tuple[float, float] = (0.9, 0.99),
-        weight_decay: float = 0.0,
-    ):
-        assert lr > 0.0
-        defaults = {lr: lr, betas: betas, weight_decay: weight_decay}
-
-        super().__init__(params, defaults)
-
-    @torch.no_grad()
-    def step(self, closure: Optional[Callable] = None):
-        loss = None
-        if exists(closure):
-            with torch.enable_grad():
-                loss = closure()
-
-        for group in self.param_groups:
-            for p in filter(lambda p: exists(p.grad), group["params"]):
-                grad, lr, wd, beta1, beta2, state = (
-                    p.grad,
-                    group["lr"],
-                    group["weight_decay"],
-                    *group["betas"],
-                    self.state[p],
-                )
-
-                # init state - exponential moving average of gradient values
-
-                if len(state) == 0:
-                    state["exp_avg"] = torch.zeros_like(p)
-
-                exp_avg = state["exp_avg"]
-
-                self.update_fn(p, grad, exp_avg, lr, wd, beta1, beta2)
-
-        return loss
 
 
 def closest_power_of_2(x):
