6b1752ba7209d791a42388474e2276b887fa2e93
Pattptr <patt.patteera@gmail.com>
2023-05-02 15:33:58 +0700

    edit decode function

--- src/model/openthaigpt_pretraining_model/models/GPTJ_TH_tokenizer/tokenizer.py
+++ src/model/openthaigpt_pretraining_model/models/GPTJ_TH_tokenizer/tokenizer.py
@@ -1,40 +1,34 @@
 from transformers import PreTrainedTokenizerFast
 from constants import GPTJ_TOKEN_DIR, NEW_TOKEN_DIR
-from typing import Union, List
+from typing import Any, List
 
 
 class GPTJToken:
     def __init__(self):
         self.tokenizer = PreTrainedTokenizerFast(tokenizer_file=GPTJ_TOKEN_DIR)
 
-    def tokenize(self, x: str) -> str:
+    def tokenize(self, x: str) -> List[Any]:
         return self.tokenizer.tokenize(x)
 
-    def encode(self, x: str) -> str:
+    def encode(self, x: str) -> List[Any]:
         return self.tokenizer.encode(x)
 
-    def decode(self, x: Union[int, List[int]]) -> str:
-        if isinstance(x, int):
-            return self.tokenizer.decode(x)
-        else:
-            return [self.tokenizer.decode(i) for i in x]
+    def decode(self, x: int) -> str:
+        return self.tokenizer.decode(x)
 
 
 class MergedToken:
     def __init__(self):
         self.tokenizer = PreTrainedTokenizerFast(tokenizer_file=NEW_TOKEN_DIR)
 
-    def tokenize(self, x: str) -> str:
+    def tokenize(self, x: str) -> List[Any]:
         return self.tokenizer.tokenize(x)
 
-    def encode(self, x: str) -> str:
+    def encode(self, x: str) -> List[Any]:
         return self.tokenizer.encode(x)
 
-    def decode(self, x: Union[int, List[int]]) -> str:
-        if isinstance(x, int):
-            return self.tokenizer.decode(x)
-        else:
-            return [self.tokenizer.decode(i) for i in x]
+    def decode(self, x: int) -> str:
+        return self.tokenizer.decode(x)
 
 
 text = "รายละเอียดและหลักเกณฑ์การคัดเลือก AI Startup Incubation​ by AIEAT"
