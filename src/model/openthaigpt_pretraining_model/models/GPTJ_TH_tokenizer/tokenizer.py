48d04eaca23aa2b72398bcad8989ea798cf9f6a9
Pattptr <patt.patteera@gmail.com>
2023-05-02 19:06:24 +0700

    add merge_tokenizer.py, add test case, modified tokenizer and constants.py

--- src/model/openthaigpt_pretraining_model/models/GPTJ_TH_tokenizer/tokenizer.py
+++ src/model/openthaigpt_pretraining_model/models/GPTJ_TH_tokenizer/tokenizer.py
@@ -1,11 +1,25 @@
 from transformers import PreTrainedTokenizerFast
-from constants import GPTJ_TOKEN_DIR, NEW_TOKEN_DIR
+from constants import GPTJ_TOKEN_DIR, TH_TOKEN_DIR, NEW_TOKEN_DIR
 from typing import Any, List, Union
 
 
 class GPTJToken:
     def __init__(self):
         self.tokenizer = PreTrainedTokenizerFast(tokenizer_file=GPTJ_TOKEN_DIR)
+
+    def tokenize(self, x: str) -> List[Any]:
+        return self.tokenizer.tokenize(x)
+
+    def encode(self, x: str) -> List[Any]:
+        return self.tokenizer.encode(x)
+
+    def decode(self, x: Union[int, List[int]]) -> str:
+        return self.tokenizer.decode(x)
+
+
+class GPT2Token:
+    def __init__(self):
+        self.tokenizer = PreTrainedTokenizerFast(tokenizer_file=TH_TOKEN_DIR)
 
     def tokenize(self, x: str) -> List[Any]:
         return self.tokenizer.tokenize(x)
@@ -31,8 +45,8 @@ class MergedToken:
         return self.tokenizer.decode(x)
 
 
-text = "รายละเอียดและหลักเกณฑ์การคัดเลือก AI Startup Incubation by AIEAT"
-tokens = MergedToken()
-print(tokens.tokenize(text))
-print(tokens.encode(text))
-print([tokens.decode([token]) for token in tokens.encode(text)])
+# text = "รายละเอียดและหลักเกณฑ์การคัดเลือก AI Startup Incubation by AIEAT"
+# tokens = MergedToken()
+# print(tokens.tokenize(text))
+# print(tokens.encode(text))
+# print([tokens.decode([token]) for token in tokens.encode(text)])
---
+++ tests/model/GPTJ_th_tokenizer_test.py
@@ -0,0 +1,35 @@
+# import time
+from openthaigpt_pretraining_model.models.GPTJ_TH_tokenizer.tokenizer import (
+    GPTJToken,
+    GPT2Token,
+    MergedToken,
+)
+
+
+TEXT_TEST_CASES = [
+    "การใช้งานหลักของ GPTJ คือการวิจัยเกี่ยวกับรูปแบบภาษาที่ใหญ่",
+    "GPTJ มุ่งเน้นที่การศึกษารูปแบบภาษาที่กว้างขวาง",
+    "อยากให้วันนี้ pull request ผ่าน",
+    "อยากกินโมโม่พาราไดซ์",
+    "รายละเอียดและหลักเกณฑ์การคัดเลือก AI Startup Incubation by AIEAT",
+]
+
+TEXT_TEST_TOKEN = [
+    "ResNets solve the famous known vanishing gradient.",
+    "Each of the layers follow the same pattern.",
+    "ResNet on the paper is mainly explained for ImageNet dataset.",
+]
+
+
+def test_merge_tokenizer():
+    gptj_token = GPTJToken()
+    gpt2_token = GPT2Token()
+    m_token = MergedToken()
+
+    for idx, test in enumerate(TEXT_TEST_CASES):
+        assert [m_token.decode([token]) for token in m_token.encode(test)] == [
+            gpt2_token.decode([token]) for token in gpt2_token.encode(test)
+        ]
+
+    for idx, token in enumerate(TEXT_TEST_CASES):
+        assert m_token.encode(token) == gptj_token.encode(token)
