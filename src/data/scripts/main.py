93069e3d15963f7ff8866f73e29189c2063981ee
Chawak <mondcha1507@gmail.com>
2023-05-03 00:46:08 +0700

    Change keywords path and add striping and sort keyword by len

--- src/data/scripts/main.py
+++ src/data/scripts/main.py
@@ -6,13 +6,19 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--input_file", help="Name of an input file", default="input.txt")
 parser.add_argument(
-    "--output_file", help="Name of an output file", default="output.txt"
+    "--input_file",
+    help='Name of an input file (Default: "input.txt")',
+    default="input.txt",
+)
+parser.add_argument(
+    "--output_file",
+    help='Name of an output file (Default: "output.txt")',
+    default="output.txt",
 )
 parser.add_argument(
     "--engine",
-    help="Engine of preprocessing (Default: core)",
+    help='Engine of preprocessing (Default: "core")',
     default="core",
     choices=["core", "oscar"],
 )
@@ -21,10 +27,10 @@ args = parser.parse_args()
 
 CLEAN_FUNCTION = {"oscar": clean_oscar_dataset, "core": clean_dataset}
 
-with open(args.input_file, "r") as f:
+with open(args.input_file, "r", encoding="utf-8") as f:
     dataset = [line.strip() for line in f.readlines()]
 
 output_dataset = CLEAN_FUNCTION[args.engine](dataset)
 
-with open(args.output_file, "w") as f:
+with open(args.output_file, "w", encoding="utf-8") as f:
     f.write("\n".join(output_dataset))
