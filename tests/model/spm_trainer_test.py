7be664d448521362b44c7f197ac649febcbfbefe
pitikorn32 <pitikorn32@tqm.co.th>
2023-05-03 14:24:49 +0700

    feat: spm_trainer custom data

---
+++ tests/model/spm_trainer_test.py
@@ -0,0 +1,16 @@
+import pytest
+
+from openthaigpt_pretraining_model.spm_trainer import train_tokenizer
+
+OUTPUT_PATH = "./tokenizer"
+VOCAB_SIZE_TEST_CASES = [5000, 7500]
+NUM_DOCS_TEST_CASES = [500, 750]
+
+
+def test_train_tokenizer():
+    for i, num_docs in enumerate(NUM_DOCS_TEST_CASES):
+        train_tokenizer(
+            output_path=OUTPUT_PATH,
+            vocab_size=VOCAB_SIZE_TEST_CASES[i],
+            num_docs=num_docs,
+        )
