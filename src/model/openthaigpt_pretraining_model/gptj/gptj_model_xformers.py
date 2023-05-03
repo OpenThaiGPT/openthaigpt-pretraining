fd88b6a4568c8c620d5149557829536c3e8f7ac2
Your Name <you@example.com>
2023-05-03 12:00:37 +0700

    use Funtion(create_sinusoidal_positions,get_embed_positions,apply_rotary_pos_emb,) from Transformers library

--- src/model/openthaigpt_pretraining_model/gptj/gptj_model_xformers.py
+++ src/model/openthaigpt_pretraining_model/gptj/gptj_model_xformers.py
@@ -32,6 +32,11 @@ from transformers.utils import (
     is_torch_fx_proxy,
     logging,
 )
+from transformers.models.gptj.modeling_gptj import (
+    create_sinusoidal_positions,
+    get_embed_positions,
+    apply_rotary_pos_emb,
+)
 from transformers.utils.model_parallel_utils import assert_device_map, get_device_map
 from transformers import GPTJConfig
 
@@ -48,36 +53,6 @@ GPTJ_PRETRAINED_MODEL_ARCHIVE_LIST = [
     "hf-internal-testing/tiny-random-gptj",
     # See all GPT-J models at https://huggingface.co/models?filter=gptj
 ]
-
-
-def create_sinusoidal_positions(num_pos: int, dim: int) -> torch.Tensor:
-    inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2) / dim))
-    sinusoid_inp = torch.einsum(
-        "i , j -> i j", torch.arange(num_pos, dtype=torch.float), inv_freq
-    ).float()
-    return torch.cat((torch.sin(sinusoid_inp), torch.cos(sinusoid_inp)), dim=1)
-
-
-# @torch.fx.wrap
-def get_embed_positions(embed_positions, position_ids):
-    return embed_positions.to(position_ids.device).repeat(position_ids.shape[0], 1, 1)
-
-
-def rotate_every_two(x: torch.Tensor) -> torch.Tensor:
-    x1 = x[:, :, :, ::2]
-    x2 = x[:, :, :, 1::2]
-    x = torch.stack((-x2, x1), dim=-1)
-    return x.flatten(
-        -2
-    )  # in einsum notation: rearrange(x,'... d j -> ... (d j)') # noqa: E501
-
-
-def apply_rotary_pos_emb(
-    tensor: torch.Tensor, sin: torch.Tensor, cos: torch.Tensor
-) -> torch.Tensor:
-    sin = torch.repeat_interleave(sin[:, :, None, :], 2, 3)
-    cos = torch.repeat_interleave(cos[:, :, None, :], 2, 3)
-    return (tensor * cos) + (rotate_every_two(tensor) * sin)
 
 
 class GPTJAttention(nn.Module):
