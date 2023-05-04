b33bd6e74072e329c22ed00afa88a279d6776273
Your Name <you@example.com>
2023-05-04 16:47:31 +0700

    use memory_efficient_attention_forward

--- src/model/openthaigpt_pretraining_model/gptj/gptj_model_xformers.py
+++ src/model/openthaigpt_pretraining_model/gptj/gptj_model_xformers.py
@@ -4,9 +4,11 @@ from transformers.models.gptj.modeling_gptj import (
     GPTJModel,
 )
 from transformers import AutoTokenizer
-from xformers.components.attention.core import scaled_query_key_softmax, bmm
+import xformers.ops as xops
 
 _attn_orig = GPTJAttention._attn
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def _attn_xformers(
@@ -17,30 +19,25 @@ def _attn_xformers(
     attention_mask=None,
     head_mask=None,
 ):
-    # compute causal mask from causal mask buffer
-    query_length, key_length = query.size(-2), key.size(-2)
-    causal_mask = self.bias[
-        :, :, key_length - query_length : key_length, :key_length  # noqa: E203
-    ]
-    attention_mask = torch.where(causal_mask, 0.0, -torch.inf)
-    # Keep the attention weights computation in fp32
-    # to avoid overflow issues
-    query = query.to(torch.float32)
-    key = key.to(torch.float32)
-    attn_weights = scaled_query_key_softmax(query, key, attention_mask)
-    attn_weights = self.attn_dropout(attn_weights)
+    if attention_mask is not None:
+        raise TypeError("Not support manual attention mask")
 
-    # Mask heads if we want to
     if head_mask is not None:
-        attn_weights = attn_weights * head_mask
-    # attn_output = bmm(attn_weights, value)
-    attn_output = bmm(attn_weights, value)
+        raise TypeError("Not support head_mask")
 
-    return attn_output, attn_weights
+    # Attention output
+    attn_output = xops.memory_efficient_attention_forward(
+        query,
+        key,
+        value,
+        xops.LowerTriangularMask(),
+    )
+
+    return attn_output, None
 
 
 def get_output(pretrained_name, use_xformers, input_text):
-    model = GPTJModel.from_pretrained(pretrained_name)
+    model = GPTJModel.from_pretrained(pretrained_name).to(device)
     if use_xformers:
         print("Use xFormers")
         GPTJAttention._attn = _attn_xformers
@@ -49,7 +46,7 @@ def get_output(pretrained_name, use_xformers, input_text):
         GPTJAttention._attn = _attn_orig
 
     tokenizer = AutoTokenizer.from_pretrained(pretrained_name)
-    inputs = tokenizer(input_text, return_tensors="pt")
-    outputs = model(**inputs)
+    inputs = tokenizer(input_text, return_tensors="pt").to(device)
+    outputs = model(inputs.input_ids)
     last_hidden_states = outputs.last_hidden_state
     return last_hidden_states
