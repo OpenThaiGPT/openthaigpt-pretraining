95c85ec932a8f145da1b97af5f2f475730ef5dff
Chawak <mondcha1507@gmail.com>
2023-06-05 17:28:24 +0700

    merge mc4,oscar and perplexity code together

--- src/data/openthaigpt_pretraining_data/core/perplexity.py
+++ src/data/openthaigpt_pretraining_data/core/perplexity.py
@@ -1,11 +1,12 @@
 import kenlm
 import math
 import numpy as np
+import pandas as pd
 import pickle
 import scipy
 import sentencepiece  # type: ignore
-from text_normalizer import normalize
-from typing import List
+from openthaigpt_pretraining_data.core.text_normalizer import normalize
+from typing import List, Dict
 
 
 class SentencesLM:
@@ -15,10 +16,10 @@ class SentencesLM:
         lm_config = kenlm.Config()
         lm_config.load_method = 2
 
-        lm_model_filename = "th.arpa.bin"
+        lm_model_filename = "openthaigpt_pretraining_data/core/th.arpa.bin"
         self.lm = kenlm.Model(str(lm_model_filename), lm_config)
         self.sp = sentencepiece.SentencePieceProcessor()
-        self.sp.load("th.sp.model")
+        self.sp.load("openthaigpt_pretraining_data/core/th.sp.model")
 
     def pp(self, log_score: float, length: int) -> float:
         """Compute perplexity score"""
@@ -39,7 +40,7 @@ class SentencesLM:
         return round(self.pp(total_pp, total_length), 1)
 
 
-classifier_filename = "decision_tree.sav"
+classifier_filename = "openthaigpt_pretraining_data/core/decision_tree.sav"
 classifier = pickle.load(open(classifier_filename, "rb"))
 
 lm = SentencesLM()
@@ -57,12 +58,12 @@ def classify_spam(text: str):
 
     log_pp_score = math.log(pp_score)
 
-    prediction = classifier.predict(np.array([log_pp_score]).reshape(1, 1))
+    prediction = classifier.predict(pd.DataFrame({"log_score": [log_pp_score]}))
 
     return prediction, log_pp_score
 
 
-def sample_score(log_scores, percentage=0.1) -> np.ndarray:
+def sample_score(log_scores: List[float], percentage: float = 0.1) -> np.ndarray:
     """Sample score to use in function sample_text_back
 
     Input : log_scores -> log of perplexity scores of texts.
@@ -78,7 +79,7 @@ def sample_score(log_scores, percentage=0.1) -> np.ndarray:
     lower_bound, upper_bound = min(log_scores), max(log_scores)
 
     mean, std = np.mean(log_scores), np.std(log_scores)
-
+    print(log_scores)
     sampled_scores = scipy.stats.truncnorm.rvs(
         (lower_bound - mean) / std,
         (upper_bound - mean) / std,
@@ -90,18 +91,25 @@ def sample_score(log_scores, percentage=0.1) -> np.ndarray:
     return sampled_scores
 
 
-def sample_text_back(texts, log_scores, percentage=0.1, replace=True) -> List[str]:
+def sample_text_back(
+    spam_data_points: List[dict[str, str]],
+    log_scores: List[float],
+    percentage: float = 0.1,
+    replace: bool = True,
+) -> List[dict]:
     """Sample some spam text back in the dataset
     using log score distribution of language model
 
-    Input : texts -> texts classified as spam by perplexity.
+    Input : spam_data_points -> data points which its text classified as spam
+                            by perplexity.
     Input : log_scores -> log of perplexity scores of texts.
     Input : percentage -> percent of data to sample back.
     Input : replace -> If True the sampled data can be duplicated.
 
     Output : sampled_texts -> The sampled texts.
     """
-
+    if len(spam_data_points) <= 1:
+        return []
     sampled_scores = sample_score(log_scores, percentage)
 
     sampled_texts = []
@@ -120,9 +128,43 @@ def sample_text_back(texts, log_scores, percentage=0.1, replace=True) -> List[st
                 min_diff = diff
                 min_idx = idx
 
-        sampled_texts.append(texts[min_idx])
+        sampled_texts.append(spam_data_points[min_idx])
 
         if not replace:
             selected_idx.add(min_idx)
 
     return sampled_texts
+
+
+def remove_spam_from_dataset(
+    dataset: List[Dict[str, str]],
+    percentage: float = 0.1,
+    replace: bool = False,
+) -> List[Dict[str, str]]:
+    """Remove spam which being classified as spam by the classifier
+       and also sample percentage percent of the spam back in the dataset
+    Input : dataset -> dataset of text contains its text in 'text' field.
+    Input : percentage -> percent of data to sample back.
+    Input : replace -> If True the sampled data can be duplicated.
+
+    Output : sampled_texts -> The sampled texts.
+    """
+    spam_data_points = []
+    spam_log_scores = []
+
+    non_spam_data_points = []
+
+    for i, data_point in enumerate(dataset):
+        prediction, log_score = classify_spam(data_point["text"])
+
+        if prediction == 1:
+            spam_data_points.append(data_point)
+            spam_log_scores.append(log_score)
+        else:
+            non_spam_data_points.append(data_point)
+
+    sampled_spam_texts = sample_text_back(
+        spam_data_points, spam_log_scores, percentage=percentage, replace=replace
+    )
+
+    return non_spam_data_points + sampled_spam_texts
--- src/data/scripts/main.py
+++ src/data/scripts/main.py
@@ -4,40 +4,68 @@ from openthaigpt_pretraining_data.mc4.preprocess import (
 from openthaigpt_pretraining_data.oscar.preprocess import (
     clean_dataset as clean_oscar_dataset,
 )
-from openthaigpt_pretraining_data.core.preprocess import clean_dataset as clean_dataset
+from openthaigpt_pretraining_data.core.perplexity import remove_spam_from_dataset
 import argparse
+import os
+import platform
+from datetime import datetime
+
+
+def creation_date(path_to_file):
+    """
+    Try to get the date that a file was created, falling back to when it was
+    last modified if that isn't possible.
+    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    """
+    if platform.system() == "Windows":
+        return datetime.fromtimestamp(os.path.getctime(path_to_file)).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return datetime.fromtimestamp(stat.st_birthtime).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
+        except AttributeError:
+            return datetime.fromtimestamp(stat.st_mtime).strftime("%Y-%m-%d %H:%M:%S")
+
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
     "--input_file",
     help='Name of an input file (Default: "input.txt")',
-    default="input.txt",
+    default="scripts/input.jsonl",
 )
 parser.add_argument(
     "--output_file",
     help='Name of an output file (Default: "output.txt")',
-    default="output.txt",
-)
-parser.add_argument(
-    "--engine",
-    help='Engine of preprocessing (Default: "core")',
-    default="core",
-    choices=["core", "oscar", "mc4"],
+    default="scripts/output.jsonl",
 )
 
 args = parser.parse_args()
 
-CLEAN_FUNCTION = {
-    "mc4": clean_mc4_dataset,
-    "oscar": clean_oscar_dataset,
-    "core": clean_dataset,
-}
-
 with open(args.input_file, "r", encoding="utf-8") as f:
-    dataset = [line.strip() for line in f.readlines()]
+    json_list = list(f)
 
-output_dataset = CLEAN_FUNCTION[args.engine](dataset)
+dataset = []
+for json_str in json_list:
+    dataset.append(eval(json_str))
+
+if "created_time" not in dataset[0]:
+    created_datetime = creation_date(args.input_file)
+    for i, data in enumerate(dataset):
+        dataset[i]["created_time"] = created_datetime
+        dataset[i]["updated_time"] = created_datetime
+
+dataset = clean_mc4_dataset(dataset)
+dataset = clean_oscar_dataset(dataset)
+dataset = remove_spam_from_dataset(dataset)
+
+for i, data in enumerate(dataset):
+    dataset[i]["source"] = ""
+    dataset[i]["source_id"] = str(i)
 
 with open(args.output_file, "w", encoding="utf-8") as f:
-    f.write("\n".join(output_dataset))
+    f.write("\n".join([str(data_point) for data_point in dataset]))
