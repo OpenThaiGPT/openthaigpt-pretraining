b0418cc2dc0f44c879e218ea3fa1865a1659991b
Norapat Buppodom <nbuppodo@x3002c0s9b0n0.head.lanta.nstda.or.th>
2023-06-20 06:58:05 +0700

    refactor: move constant to cap var

--- src/data/openthaigpt_pretraining_data/core/minhash.py
+++ src/data/openthaigpt_pretraining_data/core/minhash.py
@@ -2,11 +2,14 @@ from openthaigpt_pretraining_data.core.constants import MINHASH_SEED
 from nlpo3 import segment
 from datasketch import MinHash
 
+DEFAULT_MINHASH_COL_NAME = 'text'
+DEFAULT_NUM_PERMUTATION = 128
+N_GRAM = 5
 
 def generate_minhash_signature(text, num_perm):
     minhash = MinHash(seed=MINHASH_SEED, num_perm = num_perm)
     tokens = segment(text, "newmm")
-    n_gram = 5
+    n_gram = N_GRAM
 
     for i in range(len(tokens) - n_gram + 1):
         token_gram = "".join(tokens[i : i + n_gram])
@@ -15,6 +18,6 @@ def generate_minhash_signature(text, num_perm):
 
     return minhash
 
-def generate_minhash_signature_hf(doc, num_perm = 128, col_name = 'text'):
+def generate_minhash_signature_hf(doc, num_perm = DEFAULT_NUM_PERMUTATION, col_name = DEFAULT_MINHASH_COL_NAME):
     minhash = generate_minhash_signature(doc[col_name], num_perm)
     return {"hashvalues": minhash.hashvalues}
