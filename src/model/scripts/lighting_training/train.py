a38e79c12debbbd76553913895d89c3bfee6f2c7
Norapat Buppodom <nbuppodo@x3002c0s9b0n0.head.lanta.nstda.or.th>
2023-06-20 07:32:54 +0700

    feat: migrate to normal hydra setup

--- src/model/scripts/lighting_training/train.py
+++ src/model/scripts/lighting_training/train.py
@@ -2,25 +2,17 @@ import argparse
 from openthaigpt_pretraining_model.lightning.utils import (
     Trainer,
 )
-from openthaigpt_pretraining_model.utils import (
-    seed_everything,
-    load_hydra_config,
-)
+from openthaigpt_pretraining_model.utils import seed_everything
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--training_configuration",
-        type=str,
-        default="../configuration_example/config.yaml",
-    )
+import hydra
 
-    args = parser.parse_args()
-    print(args)
-
-    training_configuration = load_hydra_config(args.training_configuration)
-    seed_everything(training_configuration.training.seed)
+@hydra.main(version_base=None, config_path="../../configuration_example/", config_name="config")
+def main(cfg):
+    seed_everything(cfg.training.seed)
     trainer = Trainer(
-        training_configuration=training_configuration,
+        training_configuration=cfg,
     )
     trainer.train()
+
+if __name__ == "__main__":
+    main() # type: ignore
