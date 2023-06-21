961e674a6aff768defbb1e20fb10b250a5b57725
new5558 <new17353@gmail.com>
2023-06-21 16:49:53 +0700

    fix: reformat file

--- src/data/scripts/merge_jsonl/merge_jsonl.py
+++ src/data/scripts/merge_jsonl/merge_jsonl.py
@@ -2,9 +2,10 @@ import argparse
 import jsonlines
 import glob
 
+
 def merge_jsonl_files(folder_path, output_file):
     # Get a list of JSONL files in the folder
-    file_list = glob.glob(folder_path + '/*.jsonl')
+    file_list = glob.glob(folder_path + "/*.jsonl")
 
     # Create an empty list to store the merged data
     merged_data = []
@@ -15,15 +16,18 @@ def merge_jsonl_files(folder_path, output_file):
             merged_data.extend(list(reader))
 
     # Write the merged data to the output file
-    with jsonlines.open(output_file, mode='w') as writer:
+    with jsonlines.open(output_file, mode="w") as writer:
         writer.write_all(merged_data)
 
     print(f"Merged data written to {output_file}")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Merge JSONL files in a folder.')
-    parser.add_argument('folder_path', type=str, help='Path to the folder containing JSONL files')
-    parser.add_argument('output_file', type=str, help='Output file path')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Merge JSONL files in a folder.")
+    parser.add_argument(
+        "folder_path", type=str, help="Path to the folder containing JSONL files"
+    )
+    parser.add_argument("output_file", type=str, help="Output file path")
 
     args = parser.parse_args()
 
