25310c663b17d0ff1d739394e7921c5c0f39d5ce
Norapat Buppodom <nbuppodo@x3002c0s9b0n0.head.lanta.nstda.or.th>
2023-06-23 23:16:19 +0700

    format: reformat init

--- src/data/openthaigpt_pretraining_data/blind_pdpa/__init__.py
+++ src/data/openthaigpt_pretraining_data/blind_pdpa/__init__.py
@@ -9,6 +9,7 @@ def blind_pdpa(dataset_config, blind_config):
 
     if blind_config.engine == "openthaigpt":
         from openthaigpt_pdpa import blind_pdpa_text
+
         result_data = hf_dataset.map(
             lambda doc: {text_col: blind_pdpa_text(doc[text_col])},
             num_proc=blind_config.num_proc,
