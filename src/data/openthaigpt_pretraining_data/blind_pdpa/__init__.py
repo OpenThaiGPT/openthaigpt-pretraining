6cf66a49892e6ef61926dabab76cc8dec23acf85
Norapat Buppodom <nbuppodo@x3002c0s9b0n0.head.lanta.nstda.or.th>
2023-06-23 22:15:46 +0700

    fix: blind data code not working

--- src/data/openthaigpt_pretraining_data/blind_pdpa/__init__.py
+++ src/data/openthaigpt_pretraining_data/blind_pdpa/__init__.py
@@ -4,13 +4,14 @@ HF_TEXT_LABEL = {"openthaigpt": "text"}
 
 
 def blind_pdpa(dataset_config, blind_config):
-    hf_dataset = load_from_disk(dataset_config.path_name)[dataset_config.split]
+    hf_dataset = load_from_disk(dataset_config.path_name)
+    text_col = HF_TEXT_LABEL[dataset_config.key]
 
     if blind_config.engine == "openthaigpt":
         from openthaigpt_pdpa import blind_pdpa_text
 
         result_data = hf_dataset.map(
-            lambda doc: {HF_TEXT_LABEL: blind_pdpa_text(doc[HF_TEXT_LABEL])},
+            lambda doc: {text_col: blind_pdpa_text(doc[text_col])},
             num_proc=blind_config.num_proc,
         )
         result_data.save_to_disk(blind_config.save_path)
