5107b39c05f5a302db69e8aadfae9413d98c704d
unknown <chanon.utupon@gmail.com>
2023-06-27 21:12:42 +0700

    preprocess multi split

--- src/model/scripts/lighting_training/data_preprocessing.py
+++ src/model/scripts/lighting_training/data_preprocessing.py
@@ -16,28 +16,33 @@ import hydra
 def main(cfg):
     data_process_configuration = cfg.data_process
     dataset_split = data_process_configuration.get("split", None)
-    dataset_config = cfg.dataset.get(dataset_split, None)
     if dataset_config is None:
         raise NotImplementedError(f"dataset don't have split {dataset_split}")
-    dataset = get_dataset(dataset_config)
+    if isinstance(dataset_split, str):
+        dataset_split = [dataset_split]
 
-    tokenizer = load_tokenizer(cfg.model.tokenizer)
+    for split in dataset_split:
+        dataset_config = cfg.dataset.get(split, None)
 
-    dataset = dataset.map(
-        tokenize_function(tokenizer, data_process_configuration.max_tokens),
-        desc="Tokenizing...",
-        num_proc=data_process_configuration.num_proc,
-        batched=True,
-        batch_size=data_process_configuration.batch_size,
-        remove_columns=dataset.column_names,
-    )
+        dataset = get_dataset(dataset_config)
 
-    save_path = os.path.join(
-        data_process_configuration.save_path,
-        data_process_configuration.split,
-    )
+        tokenizer = load_tokenizer(cfg.model.tokenizer)
 
-    dataset.save_to_disk(save_path)
+        dataset = dataset.map(
+            tokenize_function(tokenizer, data_process_configuration.max_tokens),
+            desc=f"Tokenizing {split} ...",
+            num_proc=data_process_configuration.num_proc,
+            batched=True,
+            batch_size=data_process_configuration.batch_size,
+            remove_columns=dataset.column_names,
+        )
+
+        save_path = os.path.join(
+            data_process_configuration.save_path,
+            split,
+        )
+
+        dataset.save_to_disk(save_path)
 
 
 if __name__ == "__main__":
