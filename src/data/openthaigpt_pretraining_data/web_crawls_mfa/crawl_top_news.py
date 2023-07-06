8500408b44f7f08806925be261d923f40c856b0c
nattjn <natt.juntara@gmail.com>
2023-07-06 15:22:05 +0700

    Modify time sleep and text_response

--- src/data/openthaigpt_pretraining_data/web_crawls_mfa/crawl_top_news.py
+++ src/data/openthaigpt_pretraining_data/web_crawls_mfa/crawl_top_news.py
@@ -1,28 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 import time
 
-ROOT = 'https://www.mfa.go.th'
-TOP_STORIES_URL = 'https://www.mfa.go.th/th/page/%E0%B8%82%E0%B9%88%E0%B8%B2%E0%B8%A7%E0%B9%80%E0%B8%94%E0%B9%88%E0%B8%99?menu=5d5bd3d815e39c306002aac4'
-UNWANTED_CLASSES = [('div','d-inline-block'),('div','pt-3 col')]
-DIV_TAG = 'div'
-P_TAG = 'p'
-A_TAG = 'a'
-DATE_CLASS = 'date'
-INFO_CLASS = 'p-3 col-md-4'
-DETAIL_CLASS = 'ContentDetailstyled__ContentDescription-sc-150bmwg-4 jWrYsI mb-3'
+ROOT = "https://www.mfa.go.th"
+TOP_STORIES_URL = "https://www.mfa.go.th/th/page/%E0%B8%82%E0%B9%88%E0%B8%B2%E0%B8%A7%E0%B9%80%E0%B8%94%E0%B9%88%E0%B8%99?menu=5d5bd3d815e39c306002aac4"
+UNWANTED_CLASSES = [("div", "d-inline-block"), ("div", "pt-3 col")]
+DIV_TAG = "div"
+P_TAG = "p"
+A_TAG = "a"
+DATE_CLASS = "date"
+INFO_CLASS = "p-3 col-md-4"
+DETAIL_CLASS = "ContentDetailstyled__ContentDescription-sc-150bmwg-4 jWrYsI mb-3"
 
-def process_title_date(response):
+
+def process_title_date(text_response):
     """
     Description:
         process titles and dates for news on every page.
     Args:
-        response
+        text_response: Text response containing HTML content.
     Returns:
         news_list: A list containing titles and dates.
     """
-    soup = BeautifulSoup(response.content, 'lxml')
+    soup = BeautifulSoup(text_response, "lxml")
     info = soup.find_all(DIV_TAG, class_=INFO_CLASS)
     date_list = soup.find_all(P_TAG, class_=DATE_CLASS)
 
@@ -36,16 +36,17 @@ def process_title_date(response):
                 data.extract()
 
         # Get news titles
-        title = inf.get_text(strip=True, separator=' ')
+        title = inf.get_text(strip=True, separator=" ")
 
         # Get news dates
         for indiv_date in date_list:
-            indiv_date = indiv_date.get_text(strip=True, separator=' ')
+            indiv_date = indiv_date.get_text(strip=True, separator=" ")
 
-        news_dict = {'title': title, 'date': indiv_date}
+        news_dict = {"title": title, "date": indiv_date}
         news_list.append(news_dict)
 
     return news_list
+
 
 def get_title_date(cur_url, page_no):
     """
@@ -60,41 +61,45 @@ def get_title_date(cur_url, page_no):
     news_list = []
 
     for page in range(1, page_no + 1):
-        url = f'{cur_url}&p={page}'
+        url = f"{cur_url}&p={page}"
         res = requests.get(url)
         res.encoding = "utf-8"
 
         if res.status_code == 200:
-            processed_data = process_title_date(res)
+            processed_data = process_title_date(res.text)
             news_list.extend(processed_data)
 
-        time.sleep(3)
+        time.sleep(0.5)
 
     return news_list
 
-def process_info(response):
-  info_list = []
-  href_list = []
 
-  soup = BeautifulSoup(response.content, 'lxml')
-  info = soup.find_all(DIV_TAG, class_=INFO_CLASS)
+def process_info(text_response):
+    info_list = []
+    href_list = []
 
-  for branch in info:
-      link = branch.find(A_TAG)
-      if link:
-        href_list.append(link['href'])
+    soup = BeautifulSoup(text_response, "lxml")
+    info = soup.find_all(DIV_TAG, class_=INFO_CLASS)
 
-  for href in href_list:
-      result = requests.get(f'{ROOT}{href}')
-      content = result.text
-      soup = BeautifulSoup(content, 'lxml')   
+    for branch in info:
+        link = branch.find(A_TAG)
+        if link:
+            href_list.append(link["href"])
 
-      details = soup.find_all(DIV_TAG, class_=DETAIL_CLASS)
-      for element in details:
-          detail = element.get_text(strip=True, separator=' ')
-          info_list.append(detail)     
+    for href in href_list:
+        result = requests.get(f"{ROOT}{href}")
+        content = result.text
+        soup = BeautifulSoup(content, "lxml")
 
-  return info_list
+        details = soup.find_all(DIV_TAG, class_=DETAIL_CLASS)
+        for element in details:
+            detail = element.get_text(strip=True, separator=" ")
+            info_list.append(detail)
+
+        time.sleep(0.5)
+
+    return info_list
+
 
 def get_info(cur_url, page_no):
     """
@@ -104,27 +109,18 @@ def get_info(cur_url, page_no):
         desired url and total of pages.
     Returns:
         info_list contains details of the news
-        """
+    """
     info_list = []
 
     for page in range(1, page_no + 1):
-      url = f'{cur_url}&p={page}'
-      res = requests.get(url)
-      res.encoding = "utf-8"
+        url = f"{cur_url}&p={page}"
+        res = requests.get(url)
+        res.encoding = "utf-8"
 
-      if res.status_code == 200:
-            processed_info = process_info(res)
+        if res.status_code == 200:
+            processed_info = process_info(res.text)
             info_list.extend(processed_info)
 
-      time.sleep(3)
+        time.sleep(0.5)
 
     return info_list
-
-news_title_date = get_title_date(cur_url = TOP_STORIES_URL,page_no = 216)
-news_details = get_info(cur_url = TOP_STORIES_URL,page_no = 216)
-
-for i, data_dict in enumerate(news_title_date):
-    if i < len(news_details):
-        data_dict.update({'detail': news_details[i]})
-        
-all_news = pd.DataFrame(news_title_date)
