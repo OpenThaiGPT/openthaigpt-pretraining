64aed5a0d1d1ea38ba94339cd06acdf1bce65ae7
nattjn <natt.juntara@gmail.com>
2023-07-06 20:33:11 +0700

    Change file name and create a new one to crawl government achievements

--- src/data/openthaigpt_pretraining_data/web_crawls_mfa/crawl_gov_achievements.py
+++ src/data/openthaigpt_pretraining_data/web_crawls_mfa/crawl_gov_achievements.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import time
 
 ROOT = "https://www.mfa.go.th"
-UNWANTED_CLASSES = [("div", "d-inline-block"), ("div", "pt-3 col")]
 DIV_TAG = "div"
 P_TAG = "p"
 A_TAG = "a"
@@ -12,34 +11,28 @@ INFO_CLASS = "p-3 col-md-4"
 DETAIL_CLASS = "ContentDetailstyled__ContentDescription-sc-150bmwg-4 jWrYsI mb-3"
 
 
-def process_title_date(text_response):
+def process_response(text_response):
     """
     Description:
         process titles and dates for news on every page.
     Args:
-        text_response: Text response containing HTML content.
+        response
     Returns:
         news_list: A list containing titles and dates.
     """
-    soup = BeautifulSoup(text_response, "lxml")
-    info = soup.find_all(DIV_TAG, class_=INFO_CLASS)
-    date_list = soup.find_all(P_TAG, class_=DATE_CLASS)
-
     news_list = []
 
-    # Exclude unrelated data
-    for inf in info:
-        for tag_name, class_attributes in UNWANTED_CLASSES:
-            unwanted_data = soup.find_all(tag_name, class_=class_attributes)
-            for data in unwanted_data:
-                data.extract()
+    soup = BeautifulSoup(text_response, "lxml")
+    info = soup.find_all(DIV_TAG, class_=INFO_CLASS)
 
+    for inf in info:
         # Get news titles
-        title = inf.get_text(strip=True, separator=" ")
+        title_list = inf.find(attrs={"title": True})
+        title = title_list.get_text(strip=True)
 
         # Get news dates
-        for indiv_date in date_list:
-            indiv_date = indiv_date.get_text(strip=True, separator=" ")
+        date_list = inf.find(P_TAG, class_=DATE_CLASS)
+        indiv_date = date_list.get_text(strip=True, separator=" ")
 
         news_dict = {"title": title, "date": indiv_date}
         news_list.append(news_dict)
@@ -47,7 +40,7 @@ def process_title_date(text_response):
     return news_list
 
 
-def get_title_date(cur_url, page_no):
+def get_title_date(cur_url):
     """
     Description:
         Get data processed by the function process_response.
@@ -59,16 +52,14 @@ def get_title_date(cur_url, page_no):
     """
     news_list = []
 
-    for page in range(1, page_no + 1):
-        url = f"{cur_url}&p={page}"
-        res = requests.get(url)
-        res.encoding = "utf-8"
+    res = requests.get(cur_url)
+    res.encoding = "utf-8"
 
-        if res.status_code == 200:
-            processed_data = process_title_date(res.text)
-            news_list.extend(processed_data)
+    if res.status_code == 200:
+        processed_data = process_response(res.text)
+        news_list.extend(processed_data)
 
-        time.sleep(0.5)
+    time.sleep(0.5)
 
     return news_list
 
@@ -100,7 +91,7 @@ def process_info(text_response):
     return info_list
 
 
-def get_info(cur_url, page_no):
+def get_info(cur_url):
     """
     Description:
         get data inside a link for every pafe
@@ -111,15 +102,13 @@ def get_info(cur_url, page_no):
     """
     info_list = []
 
-    for page in range(1, page_no + 1):
-        url = f"{cur_url}&p={page}"
-        res = requests.get(url)
-        res.encoding = "utf-8"
+    res = requests.get(cur_url)
+    res.encoding = "utf-8"
 
-        if res.status_code == 200:
-            processed_info = process_info(res.text)
-            info_list.extend(processed_info)
+    if res.status_code == 200:
+        processed_data = process_info(res.text)
+        info_list.extend(processed_data)
 
-        time.sleep(0.5)
+    time.sleep(0.5)
 
     return info_list
