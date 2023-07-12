8c5ca367fea7c0f944875f782a49d0a13d44f89c
unknown <chanon.utupon@gmail.com>
2023-07-12 14:02:36 +0700

    'remove_project_name'

--- src/model/scripts/experiments/ml_foundry_mpt_experiment/data_prep.sh
+++ src/model/scripts/experiments/ml_foundry_mpt_experiment/data_prep.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1 -c 64          # Specify number of nodes and processors per task
 #SBATCH --ntasks-per-node=1 # Specify tasks per node
 #SBATCH -t 1:00:00          # Specify maximum time limit (hour: minute: second)
-#SBATCH -A lt200056         # Specify project name
+#SBATCH -A <your project name>         # Specify project name
 #SBATCH -J data_prep        # Specify job name
 
 ml purge
--- src/model/scripts/experiments/ml_foundry_mpt_experiment/train.sh
+++ src/model/scripts/experiments/ml_foundry_mpt_experiment/train.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 2 -c 64          # Specify number of nodes and processors per task
 #SBATCH --ntasks-per-node=1 # Specify tasks per node
 #SBATCH -t 1:00:00          # Specify maximum time limit (hour: minute: second)
-#SBATCH -A lt200056         # Specify project name
+#SBATCH -A <your project name>         # Specify project name
 #SBATCH -J train            # Specify job name
 
 sml purge
