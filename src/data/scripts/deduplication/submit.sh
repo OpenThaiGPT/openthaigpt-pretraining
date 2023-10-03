a1b014bcdc8da275124ee119fc7ed512732287fb
Kriangkrai Saetan <ksaetan@x3002c0s7b0n0.head.lanta.nstda.or.th>
2023-10-04 00:44:23 +0700

    conflict

--- src/data/scripts/deduplication/submit.sh
+++ src/data/scripts/deduplication/submit.sh
@@ -6,20 +6,12 @@
 #SBATCH -A lt200056
 #SBATCH -J test
 
-module purge
-module load Apptainer
+ml Miniconda3
 
-# module load Miniconda3/22.11.1-1
-# conda activate /project/lt200056-opgpth/decontamination/.conda/env
+conda deactivate
+conda activate oscar_collosal
 
 export HF_DATASETS_CACHE="/project/lt200056-opgpth/openthaigpt-refactor/.cache"
 
-proxy_server=172.23.0.21:9631
-export http_proxy=http://$proxy_server
-export HTTP_PROXY=$http_proxy
-export https_proxy=http://$proxy_server
-export HTTPS_PROXY=$https_proxy
-
-apptainer run -B /lustrefs/flash/scratch --home /project/lt200056-opgpth/openthaigpt-refactor image_sandbox python ./src/data/scripts/deduplication/deduplicate.py
-apptainer run -B /lustrefs/flash/scratch --home /project/lt200056-opgpth/openthaigpt-refactor image_sandbox python ./src/data/scripts/decontamination/decontaminate.py
-# python ./src/data/scripts/deduplication/deduplicate.py
+python ./src/data/scripts/deduplication/deduplicate.py
+python ./src/data/scripts/decontamination/decontaminate.py
