4900edda888e38af72b80ed5020b3deb7777a146
ArthurMinovsky <autsadang41@gmail.com>
2023-11-06 06:10:56 +0700

    fixed flake8 and black formatter

--- src/data/openthaigpt_pretraining_data/core/PRD_Preprocess.py
+++ src/data/openthaigpt_pretraining_data/core/PRD_Preprocess.py
@@ -1,7 +1,8 @@
 from datasets import load_dataset
-import pandas as pd
 from tqdm.auto import tqdm
 import jsonlines
+
+LABEL = ""   # Add your label here
 
 # Load the PRD_news dataset from the Hugging Face Datasets library
 dataset = load_dataset('nakcnx/prd_news')
@@ -13,12 +14,15 @@ data_text = data['text']
 
 # Write the training data to a JSONL file
 with jsonlines.open('PRD.jsonl', 'w') as writer:
-    
     for i in tqdm(range(len(data))):
-        # Create a dictionary containing the text, source, source_id, created_date, updated_date, and meta data for the current training example
-        train_dict = {'text': f'หัวข้อ: {data_name[i]}+\n+เนื้อหา: {data_text[i]}',
-                    'source': 'PRD', 'source_id': i, 'created_date': '2023-05-21',
-                    'updated_date': '2023-05-21', 'meta':  None}
-        
+        """ Create a dictionary containing the text
+        ,source, source_id,created_date,updated_date,
+        and meta datafor the current training example"""
+        train_dict = {
+            'text': f'หัวข้อ: {data_name[i]}+\n+เนื้อหา: {data_text[i]}',
+            'source': 'PRD',
+            'source_id': i,
+            'created_date': '2023-05-21',
+            'updated_date': '2023-05-21', 'meta':  LABEL}
         # Write the dictionary to the JSONL file
         writer.write(train_dict)
