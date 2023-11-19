34420d870ef0d9aa35fdb2ce09ed5fdede33c72d
unknown <chanon.utupon@gmail.com>
2023-11-19 15:53:18 +0700

    use_in_no_chrome

--- src/data/scripts/crawl_set_annual_report/scrape_set_annual.py
+++ src/data/scripts/crawl_set_annual_report/scrape_set_annual.py
@@ -21,7 +21,12 @@ def main(cfg):
         1:
     ].reset_index(drop=True)
 
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(chrome_options)
 
     for company in tqdm(companys):
         url = BASE_URL.format(company)
